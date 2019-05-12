e8fcd6431fafd44f92b9800edd3b40f4894e3922
Andy Michel <Andymic12@gmail.com>
2019-05-12 17:03:10 -0400

    fixed bug

--- data_manager.py
+++ data_manager.py
@@ -7,20 +7,26 @@ import os
 import tarfile
 from six.moves import urllib
 import pandas as pd
+import shutil as st
 
 DEFAULT_URL = 'https://raw.githubusercontent.com/ageron/handson-ml/master'
 DEFAULT_PATH = 'data'
-def fetch_housing_data(path=DEFAULT_PATH):
-    url = DEFAULT_URL + 'datasets/housing/housing.tgz
+def load_housing_data(path=DEFAULT_PATH):
+    url = DEFAULT_URL + '/datasets/housing/housing.tgz'
     path = path + '/housing'
-    if not os.path.isdir(path):
-        print('Housing data directory does not exist, downloading...')
-        os.makedirs(path)
-        tgz_path = os.path.join(path, 'housing.tgz')
-        urllib.request.urlretrieve(url, tgz_path)
-        housing_tgz = tarfile.open(tgz_path)
-        housing_tgz.extractall(path=path)
-        housing_tgz.close()
+    
+    try:
+        if not os.path.isdir(path):
+            print('Housing data directory does not exist, downloading...')
+            os.makedirs(path)
+            tgz_path = os.path.join(path, 'housing.tgz')
+            urllib.request.urlretrieve(url, tgz_path)
+            housing_tgz = tarfile.open(tgz_path)
+            housing_tgz.extractall(path=path)
+            housing_tgz.close()
+    except Exception as e:
+        print(e)
+        st.rmtree(path)
 
     csv_path = os.path.join(path, 'housing.csv')
     return pd.read_csv(csv_path)
