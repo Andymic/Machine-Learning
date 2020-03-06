38c19abc09ecbfe3116a6310e1bc5eadf22557c8
Andy Michel <Andymic12@gmail.com>
2020-03-06 12:51:46 -0500

    start classification

---
+++ classification/mnist.py
@@ -0,0 +1,28 @@
+# @Author: floki
+# @Date:   2020-03-06T12:00:28-05:00
+# @Last modified by:   floki
+# @Last modified time: 2020-03-06T12:31:28-05:00
+
+import numpy as np
+import envs
+import os
+
+def sort_by_target(mnist):
+    reorder_train = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[:60000])]))[:, 1]
+    reorder_test = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[60000:])]))[:, 1]
+    mnist.data[:60000] = mnist.data[reorder_train]
+    mnist.target[:60000] = mnist.target[reorder_train]
+    mnist.data[60000:] = mnist.data[reorder_test + 60000]
+    mnist.target[60000:] = mnist.target[reorder_test + 60000]
+
+try:
+    from sklearn.datasets import fetch_openml
+    mnist = fetch_openml('mnist_784', data_home=envs.DATA_DIR, version=1, cache=True)
+    mnist.target = mnist.target.astype(np.int8) # fetch_openml() returns targets as strings
+    sort_by_target(mnist) # fetch_openml() returns an unsorted dataset
+except ImportError:
+    from sklearn.datasets import fetch_mldata
+    mnist = fetch_mldata('MNIST original')
+mnist["data"], mnist["target"]
+
+print("Shape: ", mnist.DESCR)
